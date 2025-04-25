01afb403396c770c240d1294e62cf4e8bd552238
Turmaxx <88465473+Turmaxx@users.noreply.github.com>
2025-04-25 09:44:27 +0300

    updated flakes

--- flake.nix
+++ flake.nix
@@ -2,18 +2,29 @@
   description = "A simple nix-flake for developing my site using zola";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.${system}.default =
-        pkgs.mkShell {
-          buildInputs = with pkgs; [ zola ];
-          shellHook = '' zola --version '';
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            name = "zola dev shell";
+            buildInputs = with pkgs; [ zola ];
+            shellHook = ''
+              zola --version
+            '';
+          };
         };
     };
 }
